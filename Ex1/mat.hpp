d777848e14122f6be4072993c6b6a556afcf7ab4
RazElbaz <93310416+RazElbaz@users.noreply.github.com>
2022-03-07 17:30:26 +0200

    Add files via upload

---
+++ Ex1/mat.hpp
@@ -0,0 +1,11 @@
+#ifndef MATALA_MAT_HPP
+#define MATALA_MAT_HPP
+
+#include <string>
+
+namespace ariel{
+    std::string mat(int c, int r, char Symbol_1,char Symbol_2);
+}
+
+
+#endif //MATALA_MAT_HP
