9daa904e1f55db0bc7229d2dfb3007282e850034
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2022-03-09 09:52:11 +0200

    Add files via upload

---
+++ mat.cpp
@@ -0,0 +1,9 @@
+#include <string>
+using  namespace std;
+
+namespace ariel{
+    string mat(int col, int row, char Symbol_one,char Symbol_two ){
+        return "";
+    };
+}
+
---
+++ mat.hpp
@@ -0,0 +1,7 @@
+#include <string>
+
+namespace ariel{
+    std::string mat(int c, int r, char Symbol1,char Symbol2);
+}
+
+
